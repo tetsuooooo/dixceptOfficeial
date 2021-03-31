e0e11830fd05b7a3807efb384436bc938f537bdb
wtetsu14 <wtetsu1030@gmail.com>
2021-03-31 09:46:14 +0900

    add: Footer追加

--- dixceptOfficial/clientapp/src/App.vue
+++ dixceptOfficial/clientapp/src/App.vue
@@ -4,6 +4,7 @@
             <v-container fluid>
             <Header></Header>
             <router-view></router-view>
+            <Footer></Footer>
             </v-container>
         </v-main>
     </v-app>
@@ -12,14 +13,14 @@
 <script lang="ts">
     import Vue from 'vue';
     import Header from './components/Header.vue'
+    import Footer from './components/Footer.vue'
 
     export default Vue.extend({
         name: 'App',
-
         components: {
             Header,
+            Footer
         },
-
         data: () => ({
             
         }),
---
+++ dixceptOfficial/clientapp/src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+    
+         <v-footer
+    color="#1B5E20"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="bg_color py-4 text-center white--text"
+        cols="12"
+      >
+        <strong>dixsept Official</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class Home extends Vue {
+  data(){
+      return{
+          links:["Home","Team","Player","News","MatchSchedule","Contact"]
+      }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
