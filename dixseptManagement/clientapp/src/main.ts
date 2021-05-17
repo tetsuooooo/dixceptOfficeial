18a3c523f243b377a788d056861ea6378da9f711
wtetsu14 <wtetsu1030@gmail.com>
2021-05-18 01:15:44 +0900

    [update] 管理画面 選手登録 作成

--- dixseptManagement/clientapp/src/main.ts
+++ dixseptManagement/clientapp/src/main.ts
@@ -6,8 +6,22 @@ import vuetify from './plugins/vuetify'
 // fontAwesome読み込み
 import '@fortawesome/fontawesome-free/css/all.css'
 
-Vue.config.productionTip = true;
+import {validationObserver, validationProvider } from './common/validate'
+// import {localize, extend} from 'vee-validate';
+// import {required} from 'vee-validate/dist/rules';import ja from 'vee-validate/dist/locale/ja.json';
 
+
+// // エラーメッセージ日本語対応
+// localize('ja', ja);
+
+// // 共通で読み込むメッセージはここに記述エラーメッセージ設定単一読み込み
+// // TODO: ファイル分割したほうがいい読み込めるか謎？
+// // 共通で使用するバリデーション読み込み
+// extend('required', required);
+Vue.component('ValidationProvider', validationProvider)
+Vue.component('ValidationObserver', validationObserver)
+
+Vue.config.productionTip = true;
 new Vue({
   router,
   store,
