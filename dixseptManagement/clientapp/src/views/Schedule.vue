ded3ef53924ea107d6aafedf48c44caa8a46e7b1
wtetsu14 <wtetsu1030@gmail.com>
2021-05-19 02:00:19 +0900

    [update] スケジュールコミット忘れ

---
+++ dixseptManagement/clientapp/src/views/Schedule.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import {methods} from '@/common/commonMethod'
+@Component({
+    mixins:[methods]
+})
+export default class Schedule extends Vue{
+    
+}
+
+</script>
+
+<style scoped>
+
+</style>
